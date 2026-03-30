c1ddcb5dd891ef3cfef446a4692e34edb9852b3c
Tuan Kiet <tuankiet5274@gmail.com>
2026-03-30 12:39:10 +0700

    CB TRANG CHỦ CHO ADMIN _GIU

--- huit_project/accounts/models.py
+++ huit_project/accounts/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 import datetime
 from django.utils import timezone
-import pyotp
+import pyotp  
 
 
 class UserProfile(models.Model):
