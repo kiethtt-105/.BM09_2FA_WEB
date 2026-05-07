01e93dc78927b0dc4ae236bce2c7241fdf46c437
Tuấn Kiệt <tuankiet5274@gmail.com>
2026-05-07 19:44:45 +0700

    run local net_

--- .bat/run 8000 + ngrok.bat
+++ .bat/run 8000 + ngrok.bat
@@ -5,6 +5,7 @@ echo
 
 start cmd /k "cd /d D:\.BM09_2FA_WEB\.BM09_2FA_WEB\huit_project && call .venv\Scripts\activate && python manage.py runserver 8000"
 
+start cmd /k "cd /d D:\.BM09_2FA_WEB\.BM09_2FA_WEB\huit_project && call .venv\Scripts\activate && python manage.py runserver 0.0.0.0:8000"
 
 start cmd /k "cd /d D:\.BM09_2FA_WEB\.BM09_2FA_WEB && ngrok http 8000"
 
